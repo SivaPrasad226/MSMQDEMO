55b7b205575cbf982fd66ae85ac83544c745249b
SivaPrasad226 <62736783+SivaPrasad226@users.noreply.github.com>
2021-10-11 13:14:25 +0530

    added

---
+++ MSMQ/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace MSMQ
+{
+
+	[ServiceContract]
+	public interface IService
+	{
+		[OperationContract(IsOneWay =true)]
+		void Process(string data);
+	}
+
+	[ServiceBehavior]
+	public class MsmqService : IService
+	{
+		[OperationBehavior]
+		public void Process(string data)
+		{
+			Console.WriteLine(string.Format("Process data (0) at (1)",data,DateTime.Now));
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceHost host = new ServiceHost(typeof(MsmqService));
+			host.Open();
+			Console.WriteLine("service is ready");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ MSMQClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSMQClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Proxy.ServiceClient client = new Proxy.ServiceClient();
+			Console.WriteLine("Enter data to Process");
+			var data = Console.ReadLine();
+			while (data != "Q")
+			{
+				client.Process(data);
+				Console.WriteLine("Enter data to Process");
+				var data = Console.ReadLine();
+			}
+		}
+	}
+}
